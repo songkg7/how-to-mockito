9edbc4bc273b5d0519347e4d4623ad6fb06abf71
Haril Song <songkg7@gmail.com>
2021-05-07 14:37:28 +0900

    Test init

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+
+    private final ICalculator iCalculator;
+
+    public Calculator(ICalculator iCalculator) {
+        this.iCalculator = iCalculator;
+    }
+
+    public int sum(int x, int y) {
+        return this.iCalculator.sum(x, y);
+    }
+
+    public int minus(int x, int y) {
+        return this.iCalculator.minus(x, y);
+    }
+
+}
